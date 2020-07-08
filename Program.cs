39c6268636dc77230e3396de9ae78346523980f1
cwkchi5781 <friendsrock2004chi@gmail.com>
2020-07-07 19:00:00 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace btsclasses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
